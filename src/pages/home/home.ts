e8acf1a301791097195dc02b6c9515f7fd0a9580
Subash Basnet <subashbasnet@Subashs-iMac.local>
2017-02-07 12:37:09 +0200

    detail page added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,33 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+// importing DetailPage reference from the resp file
+import { DetailPage } from '../detail/detail';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    // defining items array to be of any types.
+    items: any[];
+    
+    constructor(public navCtrl: NavController) {
+        this.items = [];
+        for(let i = 0; i < 10; i++){
+            this.items.push({
+                text: 'Item ' + i,
+                id: i
+            });
+        }
 
-  }
+    }
+    
+    itemSelected(item){
+        this.navCtrl.push(DetailPage,{
+            item: item
+        });
+    }
 
 }
